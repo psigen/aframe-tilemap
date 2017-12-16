65f5fb4c2e2b9ad80f402ce2faae1dfbe207c398
Pras Velagapudi <psigen@gmail.com>
2017-12-16 14:55:05 -0500

    Added a working, but inefficent version of the code.

--- aframe-tilemap.js
+++ aframe-tilemap.js
@@ -1,9 +1,10 @@
 AFRAME.registerComponent('tilemap', {
   schema: {
     src: { type: 'asset' },
-    tileWidth: { type: 'number', default: 10 },
-    tileHeight: { type: 'number', default: 10 },
+    tileWidth: { type: 'number', default: 2 },
+    tileHeight: { type: 'number', default: 2 },
     origin: { type: 'vec2', default: { x: 0.5, y: 0.5 } },
+    debug: { type: 'boolean', default: true },
   },
 
   init: function() {
@@ -11,6 +12,7 @@ AFRAME.registerComponent('tilemap', {
     const el = this.el;
     const tiles = (this.tiles = {});
 
+    // Record all current tile children of this component.
     for (const child of el.children) {
       const tile = child.components.tile;
       if (tile) {
@@ -22,46 +24,66 @@ AFRAME.registerComponent('tilemap', {
     this.bake();
   },
 
+  // 1. Get image from this.data.
+  // 2. For each pixel in image.
+  // 3. If the pixel value is in this.tiles.
+  // 4. Add that tile at the corresponding position and rotation.
   bake: function() {
-    const M_TAU = 2 * Math.PI;
+    const t0 = performance.now();
+
+    const M_TAU_SCALED = 2.0 * Math.PI / 256.0;
     const tiles = this.tiles;
-    const tileimg = this.data.src;
 
-    // Get image from this.data.
-    // For each pixel in image.
-    // If the pixel value is in this.tiles.
-    // Add that tile at the corresponding position and rotation.
-    const width = tileimg.naturalWidth;
-    const height = tileimg.naturalHeight;
+    const img = this.data.src;
+    const imgWidth = img.naturalWidth;
+    const imgHeight = img.naturalHeight;
 
-    const canvas = document.createElement('canvas'); // TODO: do I need to clean this up somehow?
+    const tileWidth = this.data.tileWidth;
+    const tileHeight = this.data.tileHeight;
+    const tileOffsetX = -tileWidth * imgWidth * this.data.origin.x;
+    const tileOffsetY = -tileHeight * imgHeight * this.data.origin.y;
+
+    const canvas = document.createElement('canvas');
     const context = canvas.getContext('2d');
-    context.drawImage(tileimg, 0, 0);
-    const data = context.getImageData(0, 0, width, height).data;
+    context.drawImage(img, 0, 0);
+    const data = context.getImageData(0, 0, imgWidth, imgHeight).data;
 
     let index = 0;
-    for (let row = 0; row < height; ++row) {
-      for (let col = 0; col < width; ++col) {
+    for (let row = 0; row < imgHeight; ++row) {
+      for (let col = 0; col < imgWidth; ++col) {
         // Extract the pixel components used for the tile rasterization.
         const [r, g, b, a] = data.slice(index, index + 4);
         index += 4;
 
         // Compute the tileId and rotation associated with this tile.
         const tileId = 256 * r + g;
-        const rotation = M_TAU * (b / 256.0);
+        const rotation = M_TAU_SCALED * b;
 
         // Retrieve the appropriate tile geometry and merge it into place.
-        console.log(tileId);
         if (tileId in tiles) {
           const tile = tiles[tileId];
-          console.log(tile);
+          const tileO3D = tile.el.object3D;
+          const instanceO3D = tileO3D.clone();
+
+          instanceO3D.translateX(tileWidth * col + tileOffsetX);
+          instanceO3D.translateY(tileHeight * row + tileOffsetY);
+          instanceO3D.rotateZ(rotation);
+          instanceO3D.visible = true;
+
+          this.el.object3D.add(instanceO3D);
         }
       }
+    }
+
+    // If the debug flag is set, print timing metrics.
+    if (this.data.debug) {
+      const t1 = performance.now();
+      console.log(`Tilemap creation took ${t1 - t0} milliseconds.`);
     }
   },
 
   update: function(oldData) {
-    this.bake();
+    // TODO: Regenerate mesh if these properties change.
   },
 
   remove: function() {
@@ -69,10 +91,9 @@ AFRAME.registerComponent('tilemap', {
   },
 });
 
+// The tile component is a placeholder used to identify which <a-entity>
+// will be merged to construct the tile element of the given value.
+// Generally, these entity should also have the component visible="false".
 AFRAME.registerComponent('tile', {
   schema: { type: 'int' },
-
-  init: function() {
-    this.el.object3D.visible = false;
-  },
 });
