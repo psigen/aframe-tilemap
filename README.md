1cf2ffab90643f996be36e9c60b313938c9abf83
Pras Velagapudi <psigen@gmail.com>
2017-12-16 14:08:06 -0500

    Initial commit w/ implementation of cloning.
    This is a first version of a tilemap plugin that supports
    image tilemaps that map to an index over existing models.
    
    This initial implementation using Object3D.clone(), which
    should generally work, but is pretty inefficient.
    
    [Yes] shared geometry buffer
    [No ] single draw call
    [Yes] preserves shaders

---
+++ README.md
@@ -0,0 +1,4 @@
+# A-Frame Tilemap Component
+
+An A-Frame component that renders a static tilemap from a set of models.
+
--- aframe-tilemap.js
+++ aframe-tilemap.js
@@ -1,62 +1,99 @@
 AFRAME.registerComponent('tilemap', {
   schema: {
     src: { type: 'asset' },
-    width: { type: 'number', default: 10 },
-    height: { type: 'number', default: 10 },
+    tileWidth: { type: 'number', default: 2 },
+    tileHeight: { type: 'number', default: 2 },
+    origin: { type: 'vec2', default: { x: 0.5, y: 0.5 } },
+    debug: { type: 'boolean', default: true },
   },
 
   init: function() {
     const data = this.data;
     const el = this.el;
+    const tiles = (this.tiles = {});
 
-    this.tiles = {};
+    // Record all current tile children of this component.
     for (const child of el.children) {
       const tile = child.components.tile;
       if (tile) {
-        this.tiles[tile.data] = tile;
-        tile.setTilemap(this);
+        tiles[tile.data] = tile;
       }
     }
+
     // TODO: add event handler for new children.
+    this.bake();
   },
 
+  // 1. Get image from this.data.
+  // 2. For each pixel in image.
+  // 3. If the pixel value is in this.tiles.
+  // 4. Add that tile at the corresponding position and rotation.
   bake: function() {
-    // Get image from this.data.
-    // For each pixel in image.
-    // If the pixel value is in this.tiles.
-    // Add that tile at the corresponding position and rotation.
+    const t0 = performance.now();
+
+    const M_TAU_SCALED = 2.0 * Math.PI / 256.0;
+    const tiles = this.tiles;
+
+    const img = this.data.src;
+    const imgWidth = img.naturalWidth;
+    const imgHeight = img.naturalHeight;
+
+    const tileWidth = this.data.tileWidth;
+    const tileHeight = this.data.tileHeight;
+    const tileOffsetX = -tileWidth * imgWidth * this.data.origin.x;
+    const tileOffsetY = -tileHeight * imgHeight * this.data.origin.y;
+
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    context.drawImage(img, 0, 0);
+    const data = context.getImageData(0, 0, imgWidth, imgHeight).data;
+
+    let index = 0;
+    for (let row = 0; row < imgHeight; ++row) {
+      for (let col = 0; col < imgWidth; ++col) {
+        // Extract the pixel components used for the tile rasterization.
+        const [r, g, b, a] = data.slice(index, index + 4);
+        index += 4;
+
+        // Compute the tileId and rotation associated with this tile.
+        const tileId = 256 * r + g;
+        const rotation = M_TAU_SCALED * b;
+
+        // Retrieve the appropriate tile geometry and merge it into place.
+        if (tileId in tiles) {
+          const tile = tiles[tileId];
+          const tileO3D = tile.el.object3D;
+          const instanceO3D = tileO3D.clone();
+
+          instanceO3D.translateX(tileWidth * col + tileOffsetX);
+          instanceO3D.translateY(tileHeight * row + tileOffsetY);
+          instanceO3D.rotateZ(rotation);
+          instanceO3D.visible = true;
+
+          this.el.object3D.add(instanceO3D);
+        }
+      }
+    }
+
+    // If the debug flag is set, print timing metrics.
+    if (this.data.debug) {
+      const t1 = performance.now();
+      console.log(`Tilemap creation took ${t1 - t0} milliseconds.`);
+    }
   },
 
   update: function(oldData) {
-    const data = this.data;
-    const el = this.el;
+    // TODO: Regenerate mesh if these properties change.
   },
 
   remove: function() {
-    const data = this.data;
-    const el = this.el;
+    // Do nothing.
   },
 });
 
+// The tile component is a placeholder used to identify which <a-entity>
+// will be merged to construct the tile element of the given value.
+// Generally, these entity should also have the component visible="false".
 AFRAME.registerComponent('tile', {
   schema: { type: 'int' },
-
-  init: function() {
-    const data = this.data;
-    const el = this.el;
-  },
-
-  update: function(oldData) {
-    const data = this.data;
-    const el = this.el;
-  },
-
-  remove: function() {
-    const data = this.data;
-    const el = this.el;
-  },
-
-  setTilemap: function(tilemap) {
-    this.tilemap = tilemap;
-  },
 });
